2a9e1197509254ffca6d6c09c673a3e4438d08e4
ylambda <ylambda@koalabeast.com>
2016-06-29 17:09:49 -0400

    Ignore node_modules

--- util/twitch.js
+++ util/twitch.js
@@ -3,18 +3,19 @@ var fetch = require('node-fetch');
 const TWITCH_GAME_STREAMS_URL = 'https://api.twitch.tv/kraken/streams?game=';
 
 function fetchGameStreams(game) {
-	return fetch(TWITCH_GAME_STREAMS_URL + game)
-		.then(res => res.json())
-		.then(data => {
-			return data.streams.map(stream => {
-				return {
-					name: stream.channel.name,
-					url: stream.channel.url
-				}
-			});
-		});
+    return fetch(TWITCH_GAME_STREAMS_URL + game)
+        .then(res => res.json())
+        .then(data => {
+            return data.streams.map(stream => {
+                return {
+                    name: stream.channel.name,
+                    url: stream.channel.url,
+                    thumbnail: stream.preview.medium
+                }
+            });
+        });
 }
 
 module.exports = {
-	fetchGameStreams
+    fetchGameStreams
 }
